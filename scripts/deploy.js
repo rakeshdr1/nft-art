7ea3cb1175323613a8ea5aa1a7cd152733957555
Rakesh D R <rakesh@rapidinnovation.dev>
2022-01-19 07:41:29 +0530

    Created sample onchain svg

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,28 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
+const fs = require("fs");
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const [deployer] = await ethers.getSigners();
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  console.log("Deploying contracts with the account:", deployer.address);
 
-  await greeter.deployed();
+  console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  console.log("Greeter deployed to:", greeter.address);
+  const SVGNFT = await ethers.getContractFactory("SVGNFT");
+  const svgNft = await SVGNFT.deploy();
+
+  console.log("SVGNFT address:", svgNft.address);
+
+  console.log("Lets create NFT now!");
+  const filePath = "./img/round.svg";
+  let svg = fs.readFileSync(filePath, { encoding: "utf8" });
+  console.log(
+    `We will use ${filePath} as our SVG, and this will turn into a tokenURI. `
+  );
+  const tx = await svgNft.create(svg);
+  console.log("NFT details", await svgNft.tokenURI(0));
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
